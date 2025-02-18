fd1c32f3624ec5983b99da2b7ad59306239ed4c6
haisen-osm <khourssaoussama.1337@gmail.com>
2025-02-18 16:51:31 +0100

    still in progress, 75% done of parsing the map

--- main.c
+++ main.c
@@ -1,124 +1,153 @@
 #include "so_long.h"
 
-void	exit_error(char *err, int per)
+void exit_error(char *err, int per)
 {
 	write(1, "Error\n", 6);
 	if (err)
-		write(1, err, strlen(err));
+		write(1, err, ft_strlen(err));
 	if (per)
 		perror("");
 	exit(EXIT_FAILURE);
 }
 
-
-int count_dots(char *path)
-{
-	int i = 0;
-	int count = 0;
-	while (path[i])
-	{
-		if (path[i] == '.')
-			count++;
-		i++;
-	}
-	return (count);
-}
-
 int check_extension(char *path)
 {
-	int len = strlen(path) - 1;
-	if (count_dots(path) == 1 && path[0] == '.')
-		return 0;
-	while (len >= 0)
-	{
-		if (path[len] == '.')
-			break;
-		len--;
-	}
-	if (strcmp((path + len), EXTENSTION) == 0)
-		return 1;
-	return 0;
-}
-// 1111111111111111111111111111111111
-// 1E0000000000000C00000C000000000001
-// 1010010100100000101001000000010101
-// 1010010010101010001001000000010101
-// 1P0000000C00C0000000000000000000C1
-// 1111111111111111111111111111111111
+	int len = ft_strlen(path);
 
-void	check_arguments(int argc, char *argv[])
+	if (len < 5)
+		return (0);
+	if (strcmp(path + len - 4, ".ber") != 0)
+		return (0);
+	int i = len - 5;
+	while (i >= 0)
+	{
+		if (path[i] == '/')
+			return (i < len - 5);
+		i--;
+	}
+	return (1);
+}
+
+void check_borders(char *str, t_map *map, int is_first, int is_last)
+{
+	int i = 0;
+
+	while (str[i] && str[i] != '\n')
+	{
+		if ((is_first || is_last) && str[i] != '1')
+			map->is_valid = 0;
+		else if ((i == 0 || i == (int)map->cols - 1) && str[i] != '1')
+			map->is_valid = 0;
+
+		i++;
+	}
+}
+
+void check_player_and_coins(char *str, t_map *map)
+{
+	int i = 0;
+	while (str[i] && str[i] != '\n')
+	{
+		if (str[i] == 'P')
+			map->player++;
+		else if (str[i] == 'C')
+			map->coin++;
+		else if (str[i] == 'E')
+			map->exitDor++;
+		else if (str[i] != '0' && str[i] != '1')
+			map->extra_char++;
+		i++;
+	}
+}
+
+void check_arguments(int argc, char *argv[])
 {
 	if (argc < 2)
-		exit_error("not enough arguments.", 0);
+		exit_error(ERR_ARGS, 0);
 	if (argc > 2)
-		exit_error("too much arguments.", 0);
+		exit_error(ERR_ARGS, 0);
 	if (!check_extension(argv[1]))
-		exit_error("wrong file extension.", 0);
+		exit_error(ERR_EXT, 0);
 }
 
-typedef struct s_map
+void initialize_map(t_map *map)
 {
-	size_t rows;
-	size_t cols;
-	int is_valid;
-	int first_row;
-	int last_row;
-}	t_map;
-
-void check_first_row(char *str, t_map *map)
-{
-	int i = 0;
-	while (str[i])
-	{
-		if (str[i] != '1' && str[i] != '\n')
-			map->is_valid = 0;
-		i++;
-	}
+	map->rows = 0;
+	map->is_valid = 1;
+	map->player = 0;
+	map->coin = 0;
+	map->exitDor = 0;
+	map->extra_char = 0;
 }
 
-int	main(int argc, char *argv[])
+void check_map(t_map *map, int fd)
 {
-	t_map map;
-	check_arguments(argc, argv);
-	int fd = open(argv[1], O_RDONLY);
-	if (fd < 0)
-		exit_error(NULL, 1);
-	char *str = NULL;
-	size_t len = 0;
 	int i = 0;
-	map.rows = 0;
+	char *str;
+
 	while ((str = get_next_line(fd)) != NULL)
 	{
-		printf("%s",str);
-		len = ft_strlen(str);
-		if (*str == '\n')
-			map.is_valid = 0;
-		else if (str[len - 1] == '\n')
-		{
-			if (i == 0)
-				map.cols = len - 1;
-			else{
-				if (map.cols != len -1)
-					map.is_valid = 0;
-			}
-		}
-		else{
-			if (i == 0)
-				map.cols = len;
-			else{
-				if (map.cols != len)
-					map.is_valid = 0;
-			}
-		}
-		if (i == 0)
-			check_first_row(str, &map);
-		i++;
-		map.rows++;
-	}
-	if (map.is_valid == 0)
-		printf("not valid");
-}
+		size_t len = strlen(str);
 
-//TODO handle this case when the ".ber" is the name of the file,
-//TODO the problem is when i give the full path like this "map/.ber"
-//TODO need to check the borders.
+		if (len == 1 && str[0] == '\n')
+			exit_error(ERR_EMPTY_LINE, 0);
+
+		if (map->rows == 0)
+			map->cols = (str[len - 1] == '\n') ? len - 1 : len;
+		else if (map->cols != ((str[len - 1] == '\n') ? len - 1 : len))
+			map->is_valid = 0;
+
+		check_borders(str, map, i == 0, str[len - 1] == '\0');
+		check_player_and_coins(str, map);
+
+		free(str);
+		map->rows++;
+		i++;
+	}
+}
+int main(int argc, char *argv[])
+{
+	t_map map;
+	int fd;
+
+	check_arguments(argc, argv);
+	fd = open(argv[1], O_RDONLY);
+	if (fd < 0)
+		exit_error(ERR_OPEN, 1);
+
+	initialize_map(&map);
+	check_map(&map, fd);
+	close(fd);
+
+	if (map.rows < 3 || map.cols < 3)
+		exit_error(ERR_INVALID_MAP, 0);
+	if (!map.is_valid)
+		exit_error(ERR_INVALID_MAP, 0);
+	if (map.player != 1 || map.coin == 0 || map.exitDor != 1 || map.extra_char > 0)
+		exit_error(ERR_INVALID_MAP, 0);
+
+	map.grid = malloc((sizeof(char *) * map.rows) + 1);
+	char *str;
+	fd = open(argv[1], O_RDONLY);
+	int i = 0;
+	while ((str = get_next_line(fd)) != NULL)
+	{
+		map.grid[i] = str;
+		i++;
+	}
+	map.grid[i] = NULL;
+	i = 0;
+	int j = 0;
+	while (map.grid[i])
+	{
+		j = 0;
+		while (map.grid[i][j])
+		{
+			printf("%c", map.grid[i][j]);
+			j++;
+		}
+		i++;
+	}
+
+	return (0);
+}
--- so_long.h
+++ so_long.h
@@ -3,15 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   so_long.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: okhourss <okhourss@student.42.fr>          +#+  +:+       +#+        */
+/*   By: okhourss <okhourss@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/15 13:09:28 by okhourss          #+#    #+#             */
-/*   Updated: 2025/02/15 14:43:02 by okhourss         ###   ########.fr       */
+/*   Updated: 2025/02/18 16:20:40 by okhourss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
-# define SO_LONG_H
+#define SO_LONG_H
 
 #include <stdio.h>
 #include <string.h>
@@ -19,19 +19,37 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include <mlx.h>
+// #include <mlx.h>
+
+typedef struct s_map
+{
+	size_t rows;
+	size_t cols;
+	int is_valid;
+	int player;
+	int coin;
+	int exitDor;
+	int extra_char;
+	char **grid;
+	char **grid_copy;
+} t_map;
 
 #define EXTENSTION ".ber"
-#  define BUFFER_SIZE 13370
+#define ERR_ARGS "Invalid number of arguments."
+#define ERR_EXT "Wrong file extension."
+#define ERR_OPEN "Failed to open the file. "
+#define ERR_EMPTY_LINE "Empty line in the map."
+#define ERR_INVALID_MAP "Invalid map structure."
+#define BUFFER_SIZE 1370
 // get_next_line functions
-char	*get_next_line(int fd);
-void	*free_resources(char **saved_data, char **read_buffer);
-char	*read_and_store(int fd, char *saved_data, char *read_buffer);
-char	*extract_line_to_return(char *saved_data, char *line_to_return);
-char	*trim_saved_data(char *saved_data);
-size_t	ft_strlen(const char *s);
-char	*ft_strdup(const char *s);
-char	*ft_strjoin(char *s1, char *s2);
-char	*ft_strchr(const char *s, int c);
-char	*ft_strcpy(char *dest, const char *src);
+char *get_next_line(int fd);
+void *free_resources(char **saved_data, char **read_buffer);
+char *read_and_store(int fd, char *saved_data, char *read_buffer);
+char *extract_line_to_return(char *saved_data, char *line_to_return);
+char *trim_saved_data(char *saved_data);
+size_t ft_strlen(const char *s);
+char *ft_strdup(const char *s);
+char *ft_strjoin(char *s1, char *s2);
+char *ft_strchr(const char *s, int c);
+char *ft_strcpy(char *dest, const char *src);
 #endif
